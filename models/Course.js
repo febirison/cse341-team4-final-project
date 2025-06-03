38d34b5b2ce3dfbc45eca12a05e917662a736ad3
Jorge Sosa <joregesosa@gmail.com>
2025-06-03 14:41:37 -0400

    Add Course model with schema definition

---
+++ models/Course.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    instructor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Instructor",
+      required: true,
+    },
+    students: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student",
+      },
+    ],
+    rooms: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Course = mongoose.model("Course", courseSchema);
+module.exports = Course;
