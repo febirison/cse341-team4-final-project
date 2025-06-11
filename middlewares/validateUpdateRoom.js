71a2d00d328eb1ed00b088964c7eaf2af1ca3b90
David da Rocha Fernandes <davidrfernandes03@gmail.com>
2025-06-11 14:58:17 +0100

    Rooms Validation

---
+++ middlewares/validateUpdateRoom.js
@@ -0,0 +1,30 @@
+const validator = require('../helpers/validator');
+
+const validateUpdateRoom = (req, res, next) => {
+    const rules = {
+        buildingName: 'string',
+        roomNumber: 'string',
+        capacity: 'number',
+        'students.*': 'string',
+    };
+
+    const messages = {
+        'capacity.number': 'Capacity must be a valid number.',
+    };
+
+    const keys = Object.keys(req.body);
+
+    if (keys.length === 0) {
+        return res.status(400)
+        .json({ errors: { message: 'No fields to update.' } });
+    }
+
+    validator(req.body, rules, messages, (err, status) => {
+        if (!status) {
+            return res.status(400).json({ errors: err.errors });
+        }
+        next();
+    });
+};
+
+module.exports = validateUpdateRoom;
